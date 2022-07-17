208ed81c6557bd53fcb9a01ca2f523633861e610
TakedaRyota <tkdryt05@gmail.com>
2022-07-17 16:18:15 +0900

    スマホ高さ対応

--- js/common.js
+++ js/common.js
@@ -8,4 +8,8 @@ $(function () {
 
     $('.version-info').text(VERSION);
 
+    /* 高さvhの調整 */
+    const vh = $(window).height() * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 });
